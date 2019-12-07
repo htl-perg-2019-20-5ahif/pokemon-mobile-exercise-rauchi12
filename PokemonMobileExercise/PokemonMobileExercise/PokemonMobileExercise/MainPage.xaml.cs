9b6afa9b2ba80680e534c8340919b0b4468d3b56
Lukas Rauchenzauner <rauchi13.lr@gmail.com>
2019-12-07 12:07:01 +0100

    Parse Pokemons

---
+++ PokemonMobileExercise/PokemonMobileExercise/PokemonMobileExercise/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PokemonMobileExercise
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<PokemonDetailed> PokemonDetaileds { get; } = new ObservableCollection<PokemonDetailed>();
+
+        //****************************
+        //  TODO:
+        //
+        //  - Abilities Array parsen
+        //  - Use PokemonListPage 
+        //  - Maybe merge Pokemon and Pokemon Detailed
+        //
+        //****************************
+
+
+        private static HttpClient HttpClient = new HttpClient();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public async Task<Pokemons> GetPokemon()
+        {
+            var pokemonResponse = await HttpClient.GetAsync("https://pokeapi.co/api/v2/pokemon");
+            pokemonResponse.EnsureSuccessStatusCode();
+            var responseBody = await pokemonResponse.Content.ReadAsStringAsync();
+            var pokemons = JsonSerializer.Deserialize<Pokemons>(responseBody);
+            pokemons.Results.ForEach(p => Console.WriteLine(p.Name));
+            return pokemons;
+        }
+
+        public async Task<PokemonDetailed> GetPokemonDetailed(Pokemon pokemon)
+        {
+            var pokemonResponse = await HttpClient.GetAsync(pokemon.Url);
+            pokemonResponse.EnsureSuccessStatusCode();
+            var responseBody = await pokemonResponse.Content.ReadAsStringAsync();
+            var pokemonDetailed = JsonSerializer.Deserialize<PokemonDetailed>(responseBody);
+            return pokemonDetailed;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var pokemons = await GetPokemon();
+            foreach(var pokemon in pokemons.Results)
+            {
+                var pokemonDetailed = await GetPokemonDetailed(pokemon);
+                PokemonDetaileds.Add(pokemonDetailed);
+            }
+        }
+    }
+}
---
+++ PokemonMobileExercise/PokemonMobileExercise/PokemonMobileExercise/Pokemon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace PokemonMobileExercise
+{
+    public class Pokemon
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+    }
+
+    public class PokemonDetailed
+    {
+        [JsonPropertyName("weight")]
+        public double Weight { get; set; }
+        [JsonPropertyName("sprites")]
+        public Sprites Sprites { get; set; }
+        [JsonPropertyName("abilities")]
+        public List<Ability> Abilities { get; set; }
+    }
+    public class Pokemons
+    {
+        [JsonPropertyName("results")]
+        public List<Pokemon> Results { get; set; }
+    }
+
+    public class Ability
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+
+    public class Sprites
+    {
+        [JsonPropertyName("front_default")]
+        public string ImageFrontUrl { get; set; }
+        [JsonPropertyName("back_default")]
+        public string ImageBackUrl { get; set; }
+    }
+}
